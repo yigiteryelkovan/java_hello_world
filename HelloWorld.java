189fe32ea395a13c109acc5dbc30651e3f503e2a
yigiteryelkovan <yigiter.yelkovan@vbt.com.tr>
2021-12-02 15:13:41 +0300

    Initial Commit

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello AUCC!");
+	
+	for(int i=0; i<10; i++){
+		System.out.println((i+1)+".) Greeting!");
+	}
+	System.out.println("Good Bye AUCC!");
+  }
+}
